6a9567dd297332ccd6432169764de729dbf52a98
bahjatnsasra <bahjatnsasra@gmail.com>
2024-01-21 17:01:25 +0200

    backend

---
+++ server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+
+import 'dotenv/config'
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
+
+
+const port = process.env.PORT 
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
