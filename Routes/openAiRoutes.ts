0075513c39ec7710107a7c9c6cfaa3579fb32961
bahjatnsasra <bahjatnsasra@gmail.com>
2024-02-06 09:23:03 +0200

    creating iage

--- Routes/openAiRoutes.ts
+++ Routes/openAiRoutes.ts
@@ -7,5 +7,7 @@ const router = Router();
 router.get('/get/subjects' , openAiController.generateSubjects)
 router.get('/get/subTopics/:subject' , openAiController.generateSubSubjects)
 router.get('/get/description/:subject' , openAiController.generateDescription)
+router.get('/get/image/:subject' , openAiController.generateImage)
+
 
 export = router;
